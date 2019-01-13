fe73819451a92ea7a0d4679734f105e5f7b78f12
Evan Kemp Windows Laptop <ekp.web@gmail.com>
2019-01-13 06:17:43 +0000

    add files

---
+++ bot.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+
+TOKEN = "NTIzNzQ4NzY4NDYyNjY3Nzg2.DxQ-Nw.zMBEhIpv7kMCrVEB1zfQ7M_T0Gw"
+OWNER = 257106225777475585
+client = commands.Bot(command_prefix="!", pm_help=True)
+
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user.name}')
+
+
+@client.event
+async def on_message(message):
+    if message.author.id != client.user.id:
+        print(f'{message.author.name} said: {message.content}')
+    await client.process_commands(message)
+
+
+@client.command(pass_context=True, brief='DMs the owner of this bot.')
+async def tell_owner(ctx, *args):
+    owner_user = await client.get_user_info(OWNER)
+
+    await owner_user.send(f'{ctx.author.mention} says "{args[0]}"')
+
+
+@client.command(pass_context=True, brief='DMs a user -- message goes in quotes')
+async def message(ctx, *args):
+    user_to_message = int(args[0].replace('<@', '').replace('>', ''))
+    dm_user = await client.get_user_info(user_to_message)
+
+    await dm_user.send(f"{ctx.author.mention} wants to let you know '{args[1]}'")
+    await ctx.send('Message sent!')
+
+client.run(TOKEN)
