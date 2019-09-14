6e88d258e7d64bee573c7de7f867ee13b1126506
Evan Kemp <ekp.web@gmail.com>
2019-09-13 22:49:12 -0700

    setup lambda for db integration

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,7 @@ import requests
 import boto3
 
 from helpers.yaml_helper import read_yaml
+from helpers.database import get_event
 
 TOKEN = read_yaml('tokens')['bot_token']
 
@@ -10,30 +11,32 @@ client = boto3.client('events')
 
 def send_discord_message(event, context):
     channel = read_yaml('tokens')['test_channel']
+    db_event = get_event(event['id'])
+
     response = requests.post(f'https://discordapp.com/api/channels/{channel}/messages',
                              headers={'Authorization': f'Bot {TOKEN}', 'Content-Type': 'application/json'},
                              json={
                                  'tts': False,
                                  'embed': {
-                                    'title': event['title'],
+                                    'title': db_event['title'],
                                     'thumbnail': {
                                         'url': 'https://www.pngfind.com/pngs/m/218-2188123_3-star-citizen-logo-png-transparent-png.png'
                                     },
                                     'fields': [
                                         {
                                             'name': 'Organizer',
-                                            'value': event['user'],
+                                            'value': db_event['user'],
                                             'inline': True
                                         },
                                         {
                                             'name': 'Date/Time',
-                                            'value': f"{event['time_info']['date']} "
-                                            f"{event['time_info']['time']} \n {event['time_info']['timezone']}",
+                                            'value': f"{db_event['date']} "
+                                            f"{db_event['time']} \n {db_event['timezone']}",
                                             'inline': True
                                         },
                                         {
                                             'name': 'Description',
-                                            'value': event['description']
+                                            'value': db_event['description']
                                         }
                                     ]
                                  }
